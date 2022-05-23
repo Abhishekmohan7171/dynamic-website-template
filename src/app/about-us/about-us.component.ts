6108c8178b7cd3cdc85cf31f355680697a9d96c4
Abhishek Mohan <abhishekmohan@Abhisheks-MacBook-Pro.local>
2022-05-24 00:23:19 +0530

    fifth commit

--- src/app/about-us/about-us.component.ts
+++ src/app/about-us/about-us.component.ts
@@ -3,13 +3,19 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-about-us',
   templateUrl: './about-us.component.html',
-  styleUrls: ['./about-us.component.css']
+  styleUrls: ['./about-us.component.css'],
 })
 export class AboutUsComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  content =
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut nec lacus at dolor pharetra molestie. Pellentesque efficitur congue libero, vel maximus ligula lobortis id. Proin facilisis aliquet mauris. Nullam iaculis.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut nec lacus at dolor p aretra molestie. Pellentesque efficitur congue libero, vel maximus ligula lobortis id. Proin facilisis aliquet mauris. Nullam iaculis.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut nec lacus at dolor ph      aretra molestie. Pellentesque efficitur congue libero, vel maximus ligula lobortis id. Proin f      acilisis aliquet mauris. Nullam iaculis.Lorem ipsum dolor sit amet, consectetur adipiscing eli      t. Ut nec lacus at dolor p aretra molestie. Pellentesque efficitur congue libero, vel maximus       ligula lobortis id. Proin facilisis aliquet mauris';
 
-  ngOnInit(): void {
-  }
-
+ 
+      lineone = 'Guduvanchery,';
+      linetwo =  'Chennai, 603203';
+      email =  'xyz@email.com';
+      phone = '123456678';
+  
+  ngOnInit(): void {	}
 }

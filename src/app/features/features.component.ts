695656473d97c9fb21d9aee29f03253b2bb750aa
Abhishek Mohan <abhishekmohan@Abhisheks-MacBook-Pro.local>
2022-05-23 16:39:31 +0530

    fourth commit

--- src/app/features/features.component.ts
+++ src/app/features/features.component.ts
@@ -3,35 +3,34 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-features',
   templateUrl: './features.component.html',
-  styleUrls: ['./features.component.css']
+  styleUrls: ['./features.component.css'],
 })
 export class FeaturesComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  headline =
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut nec lacus at dolor pharetra molestie. Pellentesque efficitur congue libero, vel maximus ligula lobortis id. Proin facilisis aliquet mauris. Nullam iaculis.';
 
-	
-  headline = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut nec lacus at dolor pharetra molestie. Pellentesque efficitur congue libero, vel maximus ligula lobortis id. Proin facilisis aliquet mauris. Nullam iaculis."
+  features = [
+    {
+      Image: './assets/qr.png',
+      Heading: 'Feature 1',
+      Content:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+    },
+    {
+      Image: './assets/qr.png',
+      Heading: 'Feature 2',
+      Content:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+    },
+    {
+      Image: './assets/qr.png',
+      Heading: 'Feature 3',
+      Content:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+    },
+  ];
 
-	
- features = [
-	 {
-		 Image:"./assets/qr.png",
-		 Heading:"Feature 1",
-		 Content:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
-	 },
-	 {
-		Image:"./assets/qr.png",
-	 	Heading:"Feature 2",
-		Content:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
-	 },
-	 {
-		Image:"./assets/qr.png", 
-	 	Heading:"Feature 3",
-		Content:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
-	 }
- ];
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }

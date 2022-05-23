64e3b9bc4c79f4b73ad56d6537fb411ea64a81e1
Abhishek Mohan <abhishekmohan@Abhisheks-MacBook-Pro.local>
2022-05-23 15:30:15 +0530

    second commit

--- src/app/features/features.component.ts
+++ src/app/features/features.component.ts
@@ -9,6 +9,28 @@ export class FeaturesComponent implements OnInit {
 
   constructor() { }
 
+	
+  headline = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut nec lacus at dolor pharetra molestie. Pellentesque efficitur congue libero, vel maximus ligula lobortis id. Proin facilisis aliquet mauris. Nullam iaculis."
+
+	
+ features = [
+	 {
+		 Image:"./assets/qr.png",
+		 Heading:"Feature 1",
+		 Content:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+	 },
+	 {
+		Image:"./assets/qr.png",
+	 	Heading:"Feature 2",
+		Content:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+	 },
+	 {
+		Image:"./assets/qr.png", 
+	 	Heading:"Feature 3",
+		Content:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+	 }
+ ];
+
   ngOnInit(): void {
   }
 
